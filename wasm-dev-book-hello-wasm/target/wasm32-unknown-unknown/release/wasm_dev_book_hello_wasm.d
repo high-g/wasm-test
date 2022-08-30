dd6cb480bd929e5f4a264c81a25d093441ec33e5
high-g <altema1217bbfumi@live.jp>
2022-08-30 22:45:25 +0900

    modify: wasm build

---
+++ wasm-dev-book-hello-wasm/target/wasm32-unknown-unknown/release/wasm_dev_book_hello_wasm.d
@@ -0,0 +1,1 @@
+/Users/tani.fumiya/wasm-test/wasm-dev-book-hello-wasm/target/wasm32-unknown-unknown/release/wasm_dev_book_hello_wasm.wasm: /Users/tani.fumiya/wasm-test/wasm-dev-book-hello-wasm/src/lib.rs
