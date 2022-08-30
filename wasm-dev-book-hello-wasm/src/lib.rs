f3808310a67f66fec7f7a8c00f7b4ce3989d0a25
high-g <altema1217bbfumi@live.jp>
2022-08-30 23:12:20 +0900

    rust 3rd party tool

--- wasm-dev-book-hello-wasm/src/lib.rs
+++ wasm-dev-book-hello-wasm/src/lib.rs
@@ -11,3 +11,19 @@ extern {
 pub fn get_timestamp() -> f64 {
     unsafe { date_now() }
 }
+
+extern crate tinymt;
+
+use tinymt::tinymt32;
+
+#[no_mangle]
+pub fn rand() -> u32 {
+    let param = tinymt32::Param {
+        mat1: 0x8F7011EE,
+        mat2: 0xFC78FF1F,
+        tmat: 0x3793fdff,
+    };
+
+    let seed = 1;
+    tinymt32::from_seed(param, seed).gen()
+}
