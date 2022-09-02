0ce329419387464d861b369cc093428200dc996b
high-g <altema1217bbfumi@live.jp>
2022-09-02 22:45:28 +0900

    modify: rust 変数

--- main.rs
+++ main.rs
@@ -1,5 +1,10 @@
 fn main() {
-  let num = 1;
-  println!("hello world {} AAA", num);
+  let arg = 1;
+  println!("{}", arg);
 
+  let mut arg2 = 2;
+  println!("{}", arg2);
+
+  arg2 = 3;
+  println!("{}", arg2);
 }
