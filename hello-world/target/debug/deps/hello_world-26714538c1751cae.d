36c567631e172b9d80a838a6ebc9d82901962a0d
high-g <altema1217bbfumi@live.jp>
2022-09-02 19:24:17 +0900

    modify

---
+++ hello-world/target/debug/deps/hello_world-26714538c1751cae.d
@@ -0,0 +1,5 @@
+/Users/tani.fumiya/wasm-test/hello-world/target/debug/deps/hello_world-26714538c1751cae: src/main.rs
+
+/Users/tani.fumiya/wasm-test/hello-world/target/debug/deps/hello_world-26714538c1751cae.d: src/main.rs
+
+src/main.rs:
---
+++ hello-world/target/debug/hello-world.d
@@ -0,0 +1,1 @@
+/Users/tani.fumiya/wasm-test/hello-world/target/debug/hello-world: /Users/tani.fumiya/wasm-test/hello-world/src/main.rs
